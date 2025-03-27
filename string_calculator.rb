c9f5daca360f0ba088a505469c8dac59553c9ff1
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-03-27 16:04:13 +0530

    Updated add to find delimeter

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,5 +3,23 @@ class StringCalculator
 
         # Return 0 if empty number
 		return 0 if numbers.empty? || numbers.nil?
+
+		return numbers if numbers.is_a?(Integer)
+
+	    #Find delimeters
+		delimeter, numbers_list = get_delimeter(numbers)
+
+		
+	end
+
+	private
+
+	def get_delimeter(numbers)
+		if numbers.start_with?('//')
+		   delimiter, numbers = numbers.split("\n", 2)
+           delimiter = delimiter[2..-1]
+           numbers = numbers.gsub(delimiter, ",")
+        end
+        return delimiter, numbers
 	end
 end
