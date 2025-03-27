cb1b691271466786eb17a19c30d4c07a58b1d55c
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-03-27 15:41:30 +0530

    Added test cases for numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,4 +3,17 @@ require_relative '../string_calculator'
 RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
+   it "returns 0 for an empty string" do
+     expect(calculator.add("")).to eq(0)
+   end
+
+    it "returns the number for a single number" do
+      expect(calculator.add("3")).to eq(3)
+    end
+
+    it "returns sum of two numbers separated by a comma" do
+      expect(calculator.add("3,2")).to eq(5)
+    end
+
+
 end
