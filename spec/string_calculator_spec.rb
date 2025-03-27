b6ed253a2f85d191f1beda04586967f601f00835
DipaliBarapatre <dipalibarapatre@gmail.com>
2025-03-27 15:46:09 +0530

    Added test cases for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -31,6 +31,10 @@ RSpec.describe StringCalculator do
 		expect(StringCalculator.add("//;\n1;2")).to eq(3)
 	end
 
+	it "raises an error for negative numbers" do
+      expect { calculator.add("-1,2,-3") }.to raise_error("Negatives not allowed: -1, -3")
+    end
+
 
 
 end
